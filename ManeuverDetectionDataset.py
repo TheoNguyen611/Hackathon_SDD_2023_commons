8e216d8f5a258fb7649512ca774da07e76fb5e76
TheoNguyen611 <theo.nguyen14@gmail.com>
2023-02-26 12:59:00 +0100

    NEW Dataset parser

---
+++ ManeuverDetectionDataset.py
@@ -0,0 +1,141 @@
+import json
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+JSON_SUFFIX = ".json"
+HOUR = 3600
+DURATION_KEY = "duration_from_last_measurement"
+OBSERVATION_TIME_SPAN = 48 * HOUR
+KEY_IDENTIFIER = "keys"
+DATE_KEY = "#            Start date"
+INDEX_KEY = "Case index"
+MANEUVER_TIME_KEY = "Maneuver Time (s)"
+DELTA_V_KEY = "Delta V (m/s)"
+MINIMUM_MEASUREMENT_NB = 100
+SAMPLE_INFO_KEY = "task"
+SK_TYPE_KEY = "Sk type"
+MANEUVERS_KEY = "maneuvers"
+MAXIMUM_MEASUREMENT_COUNT_BY_SAMPLE = 1000
+RESIDUAL_KEY = "residuals"
+RIGHT_ASCENSION_KEY = "RA"
+DECLINATION_KEY = "DEC"
+
+
+
+def compute_length(dataset_type, validation_size_ratio, total_dataset_length, max_size):
+    length = total_dataset_length if max_size is None else min(max_size, total_dataset_length)
+    if dataset_type == "TRAIN":
+        return round(length * (1 - validation_size_ratio))
+    elif dataset_type == "VALIDATION":
+        return round(length * validation_size_ratio)
+    elif dataset_type == "TEST":
+        return length
+
+
+def load(dataset_path):
+    print("loading dataset. Ready in a minute!")
+    with open(dataset_path, "r+") as dataset_file:
+        return json.load(dataset_file)
+
+
+def is_maneuver(sample):
+    return 0 if OBSERVATION_TIME_SPAN < get_maneuver_info(sample)[MANEUVER_TIME_KEY] else 1
+
+
+def get_maneuver_info(sample):
+    return {DELTA_V_KEY: 0., MANEUVER_TIME_KEY: 2 * OBSERVATION_TIME_SPAN} if len(
+        sample[SAMPLE_INFO_KEY][MANEUVERS_KEY]) == 0 else sample[SAMPLE_INFO_KEY][MANEUVERS_KEY][0]
+
+
+def get_observation_time_span(file_name):
+    dates_string = file_name.split(os.path.sep)[1].split(" ")[5]
+    observation_start = dates_string.split("to")[0]
+    observation_end = dates_string.split("to")[1]
+    return observation_end, observation_start
+
+
+def get_residuals(sample, key):
+    return sample[RESIDUAL_KEY][key]
+
+
+def get_duration(sample):
+    return sample[RESIDUAL_KEY][DURATION_KEY]
+
+
+def check_arguments(dataset_type, filter_samples):
+    dataset_type_keys = {"TRAIN", "TEST", "VALIDATION"}
+    assert dataset_type in dataset_type_keys, f"Invalid 'dataset type' argument {dataset_type}. " \
+                                              f"dataset_type should be in {dataset_type_keys}"
+    filter_samples_keys = {"NO", "MANEUVER_ONLY", "WITHOUT_MANEUVER_ONLY"}
+    assert filter_samples in filter_samples_keys, f"Invalid 'filter sample' argument {filter_samples}. " \
+                                                  f"'filter sample' should be in {filter_samples_keys}"
+    if dataset_type=="TEST":
+        assert  filter_samples=="NO","Impossible to filter maneuvers, since you don't know them in the TEST dataset... " \
+                                     "nice try :P "
+
+
+def fixed_measurement_count_by_sample(dataset):
+    return len(get_residuals(dataset[dataset[KEY_IDENTIFIER][0]], RIGHT_ASCENSION_KEY))
+
+
+def parse_sample(sample, measurements_count_by_sample, time_feature):
+    feature_count = 3 if time_feature else 2
+    features = np.zeros((measurements_count_by_sample, feature_count))
+    for i, key in enumerate([RIGHT_ASCENSION_KEY, DECLINATION_KEY]):
+        residuals = get_residuals(sample, key)
+        features[:len(residuals), i] = residuals
+    if time_feature:
+        duration = get_duration(sample)
+        features[:len(duration), 2] = duration
+    return features
+
+
+def import_dataset(dataset_path, imported_dataset, filter_samples):
+    dataset = imported_dataset if imported_dataset is not None else load(dataset_path)
+    if filter_samples is "NO":
+        return dataset
+    elif filter_samples is "MANEUVER_ONLY":
+        maneuver_keys_index = [True if is_maneuver(dataset[key]) else False for key in dataset[KEY_IDENTIFIER]]
+        dataset[KEY_IDENTIFIER] = list(np.array(dataset[KEY_IDENTIFIER])[maneuver_keys_index])
+    elif filter_samples is "WITHOUT_MANEUVER_ONLY":
+        without_maneuver_keys_index = [True if not is_maneuver(dataset[key]) else False for key in
+                                       dataset[KEY_IDENTIFIER]]
+        dataset[KEY_IDENTIFIER] = list(np.array(dataset[KEY_IDENTIFIER])[without_maneuver_keys_index])
+    return dataset
+
+
+class ManeuverDetectionDataset(Dataset):
+    def __init__(self, dataset_path, dataset_type="TRAIN", validation_size_ratio=0.1, max_size=None,
+                 imported_dataset=None, filter_samples="NO", fixed_step=False, add_time_feature=True):
+        check_arguments(dataset_type, filter_samples)
+        print("\n\n**********" + dataset_type + " DATASET *********")
+        print(f"Validation/Train ratio: {validation_size_ratio}")
+        print(f"Samples filtered? {filter_samples}")
+        print(f"Samples evenly spaced? {fixed_step}")
+        self.dataset_type = dataset_type
+        self.filter_samples = filter_samples
+        self.add_time_feature = add_time_feature
+        self.dataset_path = dataset_path
+        print("path: " + self.dataset_path)
+        self.dataset = import_dataset(dataset_path, imported_dataset, filter_samples)
+        self.measurements_count_by_sample = MAXIMUM_MEASUREMENT_COUNT_BY_SAMPLE if not fixed_step \
+            else fixed_measurement_count_by_sample(self.dataset)
+        self.length = compute_length(dataset_type, validation_size_ratio, len(self.dataset[KEY_IDENTIFIER]), max_size)
+        print(f"{dataset_type} Dataset loaded. Size: {self.length}")
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, torch_loader_index):
+        dataset_index = torch_loader_index if (self.dataset_type == "TRAIN" or self.dataset_type == "TEST") else -torch_loader_index-1
+        sample_key = self.dataset[KEY_IDENTIFIER][dataset_index]
+        sample = self.dataset[sample_key]
+        features = parse_sample(sample, self.measurements_count_by_sample, self.add_time_feature)
+        if self.dataset_type=="TEST":
+            return features,0,0.,0.
+        else:
+            maneuver_info = get_maneuver_info(sample)
+            return features, is_maneuver(sample), maneuver_info[DELTA_V_KEY], maneuver_info[MANEUVER_TIME_KEY]
+
